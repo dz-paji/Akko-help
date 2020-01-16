ab00ead65216e018597c7c4dc51fe784b5a61f4f
dz_paji <zhangqian2002@outlook.com>
2020-01-16 23:29:26 +0800

    add vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'AkkoCloud - 知识库',
+  description: 'AkkoCloud 知识库，pterodactyl 面板使用手册',
+  themeConfig: {
+    docsDir: 'docs',
+    repo: 'dz-paji/Akko-help',
+    sidebar: [
+      '/',
+      '/mcpanel-explained',
+    ],
+  }
+}
